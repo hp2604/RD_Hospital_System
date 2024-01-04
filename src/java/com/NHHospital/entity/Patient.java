ab4908447378900868b891b24288cdca5876cdac
hp2604 <harshprajapati0139@gmail.com>
2024-01-04 13:20:22 +0000

    entity class and minor change

---
+++ src/java/com/NHHospital/entity/Patient.java
@@ -0,0 +1,114 @@
+package com.NHHospital.entity;
+import java.sql.Date;
+
+
+public class Patient {
+    private String patient_name,member_name,gender,contact,address,doctor_name,disease;
+    private Date appointment_date,admit_date,discharge_date;
+    private int age;
+
+    public Patient() {
+    }
+
+    public Patient(String patient_name, String member_name, String gender, String contact, String address, String doctor_name, String disease, int age) {
+        this.patient_name = patient_name;
+        this.member_name = member_name;
+        this.gender = gender;
+        this.contact = contact;
+        this.address = address;
+        this.doctor_name = doctor_name;
+        this.disease = disease;
+        this.age = age;
+    }
+
+    public String getPatient_name() {
+        return patient_name;
+    }
+
+    public void setPatient_name(String patient_name) {
+        this.patient_name = patient_name;
+    }
+
+    public String getMember_name() {
+        return member_name;
+    }
+
+    public void setMember_name(String member_name) {
+        this.member_name = member_name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDoctor_name() {
+        return doctor_name;
+    }
+
+    public void setDoctor_name(String doctor_name) {
+        this.doctor_name = doctor_name;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public Date getAppointment_date() {
+        return appointment_date;
+    }
+
+    public void setAppointment_date(Date appointment_date) {
+        this.appointment_date = appointment_date;
+    }
+
+    public Date getAdmit_date() {
+        return admit_date;
+    }
+
+    public void setAdmit_date(Date admit_date) {
+        this.admit_date = admit_date;
+    }
+
+    public Date getDischarge_date() {
+        return discharge_date;
+    }
+
+    public void setDischarge_date(Date discharge_date) {
+        this.discharge_date = discharge_date;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    
+}
